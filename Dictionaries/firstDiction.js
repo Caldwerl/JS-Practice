cadeacf7ec9525bf5b9322c5f93c2d9d46c13449
Caldwerl <caldwerl@gmail.com>
2015-04-19 15:14:15 -0400

    Add logic for newline delimited input

--- Dictionaries/firstDiction.js
+++ Dictionaries/firstDiction.js
@@ -5,6 +5,7 @@ function Dictionary () {
   this.find = find;
   this.remove = remove;
   this.showAll = showAll;
+  this.show = show;
   this.count = count;
   this.clear = clear;
 }
@@ -28,7 +29,7 @@ function count () {
 
   var n = 0;
 
-  for each (var key in this.dataStore) {
+  for (var key in this.dataStore) {
 
     n++;
   }
@@ -38,7 +39,7 @@ function count () {
 
 function clear () {
 
-  for each (var key in this.dataStore) {
+  for (var key in this.dataStore) {
 
     delete this.dataStore[key];
   }
@@ -46,9 +47,21 @@ function clear () {
 
 function showAll () {
 
-  for each (var key in this.dataStore) {
+  var count = this.count();
+  var keys = [];
 
-    print(key + " -> " + this.dataStore[key]);
+  if (count == 0) {
+    print("This dictionary is empty.");
+  }
+
+  for (var key in this.dataStore) {
+
+    keys.push(key);
+  }
+
+  for (var i = 0; i < count; i++) {
+
+    print(keys[i] + " -> " + this.dataStore[keys[i]]);
   }
 }
 
@@ -56,3 +69,45 @@ function show (key) {
 
   print(key + " -> " + this.find(key));
 }
+
+
+
+
+
+
+var inputString = "Stannis 111\nGeralt 254\nMance 909\nDredd 823";
+
+var parsedStrings = [];
+var tempString = "";
+
+var phoneDict = new Dictionary();
+
+parsedStrings = inputString.split("\n");
+
+while (parsedStrings.length > 0) {
+
+  tempString = parsedStrings.splice(0, 1).toString().split(" ");
+
+  phoneDict.add(tempString[0], tempString[1]);
+}
+
+print("Showing all records...");
+phoneDict.showAll();
+
+print("Searching for Dredd...");
+phoneDict.show("Dredd");
+
+print("Removing Dredd...");
+phoneDict.remove("Dredd");
+
+print("Adding Baygan...");
+phoneDict.add("Baygan", 555);
+
+print("Showing all records...");
+phoneDict.showAll();
+
+print("Clearing records...");
+phoneDict.clear();
+
+print("Showing all records...");
+phoneDict.showAll();
